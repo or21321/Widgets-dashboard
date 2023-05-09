1cb12211bac13b94d227516656449e1e24c276f0
ohadar <ohadar@kramerav.com>
2023-05-09 12:13:00 +0300

    Angular-gridster demo scaffold, basic demo is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { GridsterModule} from "angular-gridster2";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, GridsterModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
